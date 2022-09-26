14a423ec928f1b6f31c5825ad271503748dfa3ab
Piotr Szewello <pszewello@slb.com>
2022-09-26 15:18:58 +0200

    Lab 9

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
-const games = [
+import { Game } from '@bg-hoard/api/util-interface';
+
+const games: Array<Game> = [
   {
     id: 'settlers-in-the-can',
     name: 'Settlers in the Can',
--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { getAllGames } from '../fake-api';
 import { formatRating } from '@bg-hoard/store/util-formatters';
 
 import { HttpClient } from '@angular/common/http';
