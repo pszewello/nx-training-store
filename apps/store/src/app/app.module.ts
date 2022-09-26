4dfdacc7454a8c8a142bcbd7e157d4e6e234b532
Piotr Szewello <pszewello@slb.com>
2022-09-26 11:03:36 +0200

    Lab 3.1 -> post migrations (generated labs code)

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
-
 import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
-    MatCardModule
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
