049ca4a5061943eeab8094da35031fb61d2252f1
Piotr Szewello <pszewello@slb.com>
2022-09-26 11:52:08 +0200

    Lab 4 - import component to app

--- libs/store/ui-shared/src/lib/header/header.component.ts
+++ libs/store/ui-shared/src/lib/header/header.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'bg-hoard-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+export class HeaderComponent {
+  @Input() title = '';
 }
